6bb8dafd5c8738b31096dc7f83c6789618891188
Ludovic Vanasse <ludovicvanasse@gmail.com>
2019-04-25 19:09:48 -0400

    Change img_rgb type to CV_8UC3

--- app/PlayerThread/PlayerThread.cpp
+++ app/PlayerThread/PlayerThread.cpp
@@ -54,7 +54,7 @@ int main()
     // Define frame
     std::vector<uint8_t> frame(cam.width() * cam.height());
     Mat img_gray(cam.height(), cam.width(), CV_8UC1, frame.data());
-    Mat img_rgb(cam.height(), cam.width(), CV_8UC1);
+    Mat img_rgb(cam.height(), cam.width(), CV_8UC3);
 
     //applyColorMap(img_gray, img_rgb, COLORMAP_JET);
     // Stream frames
